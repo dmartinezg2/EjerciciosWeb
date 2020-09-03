5a6db9128bef8bda28151482c1ff30d086e6c375
dmartinezg2 <53918675+dmartinezg2@users.noreply.github.com>
2020-09-03 16:25:21 -0500

    Primer ejercicio completo

---
+++ tarea4_EjerciciosBasicosJS/main.js
@@ -0,0 +1,30 @@
+{
+    function encrypt(arr,key){ 
+    var rta=[];
+    arr.forEach(element => rta.push(element+key));
+    return rta;  
+    }
+    
+    function decrypt(arr,key){
+      arr.forEach(element => element-=key);
+      return arr;  
+    }
+    
+    function secret(arreglo,func,llave){
+        if(func=="encrypt"){
+            return encrypt(arreglo,llave);
+        }
+        else if(func=="decrypt"){
+           return decrypt(arreglo, llave);
+        }
+        else
+        {
+            console.log("Introduzca una función valida");
+        }
+    }
+    var arr = [1,2,3,4,5];
+    var llave=1;
+   
+    return "El arreglo encriptado es: "+ secret(arr,"encrypt",llave);
+    /* return "El arreglo desencriptado es: "+ secret(arr,"decrypt",llave);*/
+}
